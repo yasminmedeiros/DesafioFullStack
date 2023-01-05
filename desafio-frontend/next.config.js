a58b2496efe50cc94a1741e42be950f27ee24d97
Yasmin Medeiros <yasmin.medeirosapp@gmail.com>
2023-01-05 09:29:10 -0300

    Adicionando o Infobar

--- desafio-frontend/next.config.js
+++ desafio-frontend/next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withTM = require('next-transpile-modules')(['@impulsogov/design-system']);
+
+module.exports = withTM({
+  reactStrictMode: true,
+  swcMinify: true,
+})
